7e3c82f5d2427c7041ea417f66e65f6559103c46
Anran Zhang <zhanganran2011@Live.com>
2018-05-19 16:13:53 -0300

    newUI
    newUI

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView TextView = (TextView)findViewById(R.id.textView);
+        final EditText e = (EditText)findViewById(R.id.editText);
+        final Button button;
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(View v) {
+                                          TextView.setText(e.getText());
+                                      }
+                                  });
     }
 }
